8c280c6b303a8242f11c6d0ae1e43fd71c3cda94
huxj200 <huxj1441105278@outlook.com>
2022-06-29 13:48:01 +0800

    first commit

---
+++ next.config.js
@@ -0,0 +1,5 @@
+module.exports = {
+  env: {
+    DB_URL: "mongodb+srv://huxj:Huxj123456@cluster0.ym1wt.mongodb.net/memo?retryWrites=true&w=majority"
+  }
+}
---
+++ pages/api/login.js
@@ -0,0 +1,31 @@
+import cookie from 'cookie';
+import { fetchJson } from '../../lib/api';
+
+
+
+async function handleLogin(req, res) {
+    if (req.method !== 'POST') {
+        res.status(405).end();
+        return;
+    }
+    const { email, password } = req.body;
+    try {
+        const { jwt, user } = await fetchJson('http://localhost:1337/auth/local', {
+            method: 'POST',
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify({ identifier: email, password }),
+        });
+        res.status(200)
+            .setHeader('Set-Cookie', cookie.serialize('jwt', jwt, {
+                path: '/api',
+                httpOnly: true,
+            }))
+            .json({
+                id: user.id,
+                name: user.username,
+            });
+    } catch (err) {
+        res.status(401).end();
+    }
+}
+export default handleLogin;
